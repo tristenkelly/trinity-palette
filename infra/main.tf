53cbf167bcb2ae0fb4611ff42e2419b494e80a43
tristenkelly <tristenkelly98@gmail.com>
2025-09-17 18:51:25 -0400

    we're terraforming

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+terraform {
+    #Comment out backend and run init/plan/apply to create all resources, run init apply again afterward to use the backend
+    backend "s3" {
+        bucket         = "the-trinity-pallette-terraform-state"
+        key            = "tf-infra/terraform.tfstate"
+        region         = "us-east-1"
+        dynamodb_table = "terraform-state-locking"
+        encrypt        = true
+    }
+    required_version = ">= 1.3.0"
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.0"
+        }   
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "tf-state" {
+    source = "./modules/tf-state"
+    bucket_name = "the-trinity-pallette-terraform-state"
+}
