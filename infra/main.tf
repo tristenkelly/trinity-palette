6d406bfb7e790b1d7dc002bcdc0a05deef3afb9e
tristenkelly <tristenkelly98@gmail.com>
2025-09-17 19:52:04 -0400

    fix failing tests

--- infra/main.tf
+++ infra/main.tf
@@ -1,35 +1,35 @@
 terraform {
-    #Comment out backend and run init/plan/apply to create all resources, run init apply again afterward to use the backend
-    backend "s3" {
-        bucket         = "the-trinity-pallette-terraform-state"
-        key            = "tf-infra/terraform.tfstate"
-        region         = "us-east-1"
-        dynamodb_table = "terraform-state-locking"
-        encrypt        = true
+  #Comment out backend and run init/plan/apply to create all resources, run init apply again afterward to use the backend
+  backend "s3" {
+    bucket         = "the-trinity-pallette-terraform-state"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
-    required_version = ">= 1.3.0"
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 3.0"
-        }   
-    }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 module "tf-state" {
-    source = "./modules/tf-state"
-    bucket_name = "the-trinity-pallette-terraform-state"
+  source      = "./modules/tf-state"
+  bucket_name = "the-trinity-pallette-terraform-state"
 }
 
 module "vpc-infra" {
-    source = "./modules/vpc"
+  source = "./modules/vpc"
 
-    vpc_cidr = local.vpc_cidr
-    availability_zones = local.availability_zones
-    public_subnet_cidrs = local.public_subnet_cidrs
-    private_subnet_cidrs = local.private_subnet_cidrs
+  vpc_cidr             = local.vpc_cidr
+  availability_zones   = local.availability_zones
+  public_subnet_cidrs  = local.public_subnet_cidrs
+  private_subnet_cidrs = local.private_subnet_cidrs
 }
